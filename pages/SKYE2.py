99164c1fd70cb99c308e3dc6967b58edf038acb4
luckybenitez8-ctrl <luckybenitez8@gmail.com>
2025-11-06 18:45:43 -0500

    Update SKYE2.py

--- pages/SKYE2.py
+++ pages/SKYE2.py
@@ -4,10 +4,10 @@ from datetime import date
 
 st.set_page_config(page_title="Event Planner AI Chatbot", layout="wide")
 
-st.title("🎯 Smart Event Planner Chatbot")
+st.title("Smart Event Planner Chatbot")
 
 st.markdown("""
-Welcome to your interactive event planning assistant powered by AI!
+Hi I'm SKYE, your interactive event planning assistant powered by AI!
 
 Ask anything like:
 - *"Can I plan an outdoor birthday party this Saturday in San Diego?"*
